9a328e1fd9f3c9f2fc4f67bc05c596d1bc673b00
Jesús Manuel Martinez <jmanuelmartinez2003@gmail.com>
2024-07-07 21:23:58 -0300

    Ultimate Update Interfaces

--- Interfaces/src/org/jesus/poointerfaces/imprenta/EjemploImprenta.java
+++ Interfaces/src/org/jesus/poointerfaces/imprenta/EjemploImprenta.java
@@ -1,7 +1,7 @@
 package org.jesus.poointerfaces.imprenta;
 
 import org.jesus.poointerfaces.imprenta.modelo.*;
-import static org.jesus.poointerfaces.imprenta.modelo.Genero.*;
+import static org.jesus.poointerfaces.imprenta.modelo.Imprimible.*;
 
 public class EjemploImprenta {
     public static void main(String[] args) {
@@ -14,19 +14,13 @@ public class EjemploImprenta {
                 .addExperiencia("Angular");
 
         Informe informe = new Informe("Estudiante de microservicios",new Persona("Jesus Manuel"," Martinez"),"James");
-        imprimir(cv);
-        imprimir(informe);
+        Imprimible.imprimir(cv);
+        Imprimible.imprimir(informe);
 
 
-        Libro libro = new Libro(new Persona("Jesus Manuel"," Martinez"),"Patron de Diseños: Elem, Reusables POO", PROGRAMACION);
-        libro.addPaginas(new Pagina("Patron Singleton"))
-                .addPaginas(new Pagina("Patron Observador"))
-                .addPaginas(new Pagina("Patron Factory"))
-                .addPaginas(new Pagina("Patron Composite"))
-                .addPaginas(new Pagina("Patron Fascade"));
+
+        System.out.println(TEXTO_DEFECTO);
 
     }
-    public static void imprimir(Imprimible imprimible){
-        System.out.println(imprimible.imprimir());
-    }
+
 }
--- Interfaces/src/org/jesus/poointerfaces/imprenta/modelo/Imprimible.java
+++ Interfaces/src/org/jesus/poointerfaces/imprenta/modelo/Imprimible.java
@@ -1,8 +1,12 @@
 package org.jesus.poointerfaces.imprenta.modelo;
 
 public interface Imprimible {
-    default String imprimir(){
-        return "Imprimiendo valor por defecto.";
-    }
 
+    String TEXTO_DEFECTO = "Imprimiendo valor por defecto.";
+    default String imprimir(){ // en este caso deberia ser obligatorio hacer polimorfismo pero lo dejamos como ejemplo
+        return TEXTO_DEFECTO;
+    }
+    static void imprimir(Imprimible imprimible){
+        System.out.println(imprimible.imprimir());
+    }
 }
